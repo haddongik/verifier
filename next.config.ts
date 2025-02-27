feec1315c8bded61176f02cbdaa83167e0916e08
haddongik <haddongik@gmail.com>
2025-02-27 11:31:28 +0900

    빌드 설정

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,  // 빌드 중 ESLint 검사 무시
+  },
   /* config options here */
 };
 
